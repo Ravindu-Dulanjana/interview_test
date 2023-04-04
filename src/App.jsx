408b0be4c60338bcbdd57cabadfddd00f0e7627d
Ravindu-Dulanjana <krdulanjana@gmail.com>
2023-04-04 10:39:46 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+        <Footer />
       </Router>
       <ToastContainer />
     </>
--- src/layouts/UserLayout.jsx
+++ src/layouts/UserLayout.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Headers from "../components/Headers";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
 
 function UserLayout() {
   return (
     <>
       <Headers />
       <Outlet />
-      <Footer />
     </>
   );
 }
